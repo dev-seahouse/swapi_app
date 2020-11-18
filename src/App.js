646a622f5461f539170c859fcfeebb41e454ab73
Kenan <xinkenan.11@hotmail.com>
2020-11-18 14:15:57 +0800

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import './App.css';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <Router basename="/people">
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <div className="">
+              <h1>People page</h1>
+            </div>
+          </Route>
+          <Route path="/:personId">
+            <div className="">
+              <h1>People Details</h1>
+            </div>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
