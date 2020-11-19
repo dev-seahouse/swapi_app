984eeba3eed9a26b2ca12d209ab602212322172b
Kenan <xinkenan.11@hotmail.com>
2020-11-19 22:17:38 +0800

    added assets

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from './app/redux/store';
-import App from './App.js';
+import App from './app.js';
 
 test('renders learn react link', () => {
   const { getByText } = render(
