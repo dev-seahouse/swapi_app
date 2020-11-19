309b18fd51e0e97de7707df98957fa118779e58a
Kenan <xinkenan.11@hotmail.com>
2020-11-19 15:43:28 +0800

    added error handling for swapi

--- src/app/utils/swapi.js
+++ src/app/utils/swapi.js
@@ -1,24 +1,37 @@
 import { SWAPI_URL } from '../../config';
 
 export const getAllPeople = async () => {
-  const firstRes = await fetch(`${SWAPI_URL}/people/`);
-  const firstJson = await firstRes.json();
+  try {
+    const firstRes = await fetch(`${SWAPI_URL}/people/`);
+    const firstJson = await firstRes.json();
 
-  const numPagesToFetch = Math.ceil(firstJson.count / 10);
+    if (!firstRes.ok) {
+      throw new Error(firstRes.statusText);
+    }
 
-  const otherPromises = [];
-  for (let i = 2; i <= numPagesToFetch; i++) {
-    otherPromises.push(fetch(`${SWAPI_URL}/people/?page=${i}`));
+    const numPagesToFetch = Math.ceil(firstJson.count / 10);
+
+    const otherPromises = [];
+
+    for (let i = 2; i <= numPagesToFetch; i++) {
+      otherPromises.push(fetch(`${SWAPI_URL}/people/?page=${i}`));
+    }
+    const otherRes = await Promise.all(otherPromises);
+
+    const otherJsons = await Promise.all(otherRes.map((r) => r.json()));
+
+    if (!otherRes.ok) {
+      throw new Error(firstRes.statusText);
+    }
+
+    const combinedResults = otherJsons.reduce(
+      (acc, curr) => acc.concat(curr.results),
+      []
+    );
+    return firstJson.results.concat(combinedResults);
+  } catch (err) {
+    return Promise.reject(err.message ? err.message : []);
   }
-  const otherRes = await Promise.all(otherPromises);
-  const otherJsons = await Promise.all(otherRes.map((r) => r.json()));
-
-  const combinedResults = otherJsons.reduce(
-    (acc, curr) => acc.concat(curr.results),
-    []
-  );
-
-  return firstJson.results.concat(combinedResults);
 };
 
 export const getPeopleByPage = async (pageNum = 1) => {
