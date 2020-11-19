ff5448bcd0738226a5f6bd21795176b7b3d50154
Kenan <xinkenan.11@hotmail.com>
2020-11-19 19:29:22 +0800

    added display people names

--- src/components/people/peopleList.component.js
+++ src/components/people/peopleList.component.js
@@ -1,15 +1,27 @@
 import './peopleList.styles.scss'
 import React, {useEffect} from 'react';
-import {useSelector} from "react-redux";
-import {selectAllPeople} from "../../redux/people/peopleSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchAllPeople, selectAllPeople} from "../../redux/people/peopleSlice";
 
-export const People = () =>{
+export const People = () => {
   const allPeople = useSelector(selectAllPeople);
-  const peopleStatus = useSelector(state=>state.people)
+  const peopleStatus = useSelector(state=>state.people.status)
+  const dispatch = useDispatch()
+
   useEffect(()=>{
-    console.log(allPeople)
-  })
+      if (peopleStatus === 'idle') {
+        dispatch(fetchAllPeople())
+      }
+  }, [peopleStatus, dispatch])
+
+  let peopleList = '<p>Loading....</p>'
+  if (peopleStatus === "succeeded") {
+    peopleList = (<ul>
+      {allPeople.map(person=> (<li key={person.url}>{person.name}</li>))}
+    </ul>)
+  }
+  console.log(peopleStatus)
   return (<div>
-      <h1>People List</h1>
+    {peopleList}
   </div>)
 }
