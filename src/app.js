1db09d13c906c4f3b67aa36e8fcadb085e1f4684
Kenan <xinkenan.11@hotmail.com>
2020-11-23 15:06:10 +0800

    refactored routes to look cleaner

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import './app.scss';
-import { People } from './components/people/peopleList.component';
-import { HomePage } from './pages/home.page';
-import {PersonPage} from "./pages/person.page";
+import "./app.scss";
+import { HomePage } from "./pages/home.page";
+import { PersonPage } from "./pages/person.page";
 
 function App() {
   return (
@@ -17,15 +16,11 @@ function App() {
       <Router>
         <main>
           <Switch>
-            <Route path="/" exact>
+            <Route exact path="/">
               <Redirect to="/people" />
             </Route>
-            <Route path="/people" exact>
-              <HomePage />
-            </Route>
-            <Route path="/people/:personId">
-              <PersonPage />
-            </Route>
+            <Route exact path="/people" component={HomePage} />
+            <Route path="/people/:personId" component={PersonPage} />
           </Switch>
         </main>
       </Router>
